7257142f84e4257d35b166e00c3efe4ced9185eb
anisha-anni <anisha.anni93@gmail.com>
2023-01-30 03:11:02 +0530

    route file updated

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\BookController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', [BookController::class, 'bookRequest']);
 Route::group([], function(){
     Route::prefix('/')->group(function(){
         Route::get('add-fake-books', [BookController::class, 'addFakeBooks']);
